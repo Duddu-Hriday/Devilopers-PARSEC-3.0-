97a777412f36234ea58160db9fa47cb2dc0cae55
Duddu-Hriday <128887664+Duddu-Hriday@users.noreply.github.com>
2023-03-26 01:31:01 +0530

    Add files via upload

---
+++ medic.sql
@@ -0,0 +1,144 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 25, 2023 at 08:30 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `medic`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appoint`
+--
+
+CREATE TABLE `appoint` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `pnumber` int(11) NOT NULL,
+  `gender` varchar(255) NOT NULL,
+  `dob` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `diabetes` varchar(255) NOT NULL,
+  `report` varchar(400) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booked`
+--
+
+CREATE TABLE `booked` (
+  `id` int(11) NOT NULL,
+  `docname` varchar(255) NOT NULL,
+  `patname` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctors`
+--
+
+CREATE TABLE `doctors` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `specialization` varchar(255) NOT NULL,
+  `avail_time` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `my_admin`
+--
+
+CREATE TABLE `my_admin` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `my_admin`
+--
+
+INSERT INTO `my_admin` (`id`, `name`, `password`) VALUES
+(1, 'Admin', 'Devilopers@123');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `appoint`
+--
+ALTER TABLE `appoint`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `booked`
+--
+ALTER TABLE `booked`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `doctors`
+--
+ALTER TABLE `doctors`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `my_admin`
+--
+ALTER TABLE `my_admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `appoint`
+--
+ALTER TABLE `appoint`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `booked`
+--
+ALTER TABLE `booked`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `doctors`
+--
+ALTER TABLE `doctors`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `my_admin`
+--
+ALTER TABLE `my_admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
